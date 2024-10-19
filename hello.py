c2821f47be73d8f8e5900cdc7d36d7e23b05112b
Tony Ede <tonyede74@gmail.com>
2024-10-19 10:29:04 +0100

    Added sqrt

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 msg = "Roll a dice!!"
 print(msg)
@@ -7,3 +8,7 @@ msg = msg.upper()
 print(msg)
 
 print(np.random.randint(1,25))
+
+#Determine root 2
+rt2 = (10/7)*( 1 - 10**-2 - (1/2)*10**-4 - (1/2)*10**-6 )
+print(rt2 - math.sqrt(2))
